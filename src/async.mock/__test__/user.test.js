f7458a7372b69088ac5d2e15b295c12b703a115e
dai-yu <1435480344@qq.com>
2020-11-13 18:00:28 +0800

    finish asynv.mock test

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,5 @@
-import { register } from "../user";
+import { validate, register } from "../user";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -6,9 +7,17 @@ jest.mock("axios");
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    const result = validate("userName", "password");
+
+    expect(result).toBeTruthy();
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyPassword.mockReturnValue(false);
+
+    const result = register("userName", "password");
+
+    await expect(result).rejects.toThrow(expect.any(Error));
   });
 });
